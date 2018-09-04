16eb0bb2a31d43ccd8d84cf7f9fd19f70add9078
Landin Martens <LandinM@iqmetrix.com>
2018-09-03 21:01:35 -0500

    Transfer of base extension

---
+++ Extensions/Chrome/js/extension/Initialize.js
@@ -0,0 +1,143 @@
+chrome.extension.sendMessage({}, function (response) {
+	var readyStateCheckInterval = setInterval(function()
+	{
+		if (document.readyState === "complete")
+        {
+            clearInterval(readyStateCheckInterval);
+
+            chrome.storage.sync.get([
+                "DndBeyond_Enabled",
+                "DndBeyond_SlackEnabled",
+                "DndBeyond_SlackToken",
+                "DndBeyond_SlackChannel",
+                "DndBeyond_CharactersEnabled",
+                "DndBeyond_MonstersEnabled",
+                "DndBeyond_DiceRollerWidgetEnabled",
+                "DndBeyond_DiceRollerWidgetVerticalEnabled",
+                "DndBeyond_BetaCode"], function (data) {
+                    
+                defaultSettingsObject(data);
+
+                GLOBAL_SETTINGS_OBJECT = data;
+
+                if (GLOBAL_SETTINGS_OBJECT.DndBeyond_Enabled === true) {
+                    $.ajax({
+                        contentType: 'application/json',
+                        dataType: 'json',
+						url: "https://localhost:44335/v3/resources",
+                        method: 'POST',
+                        processData: false,
+                        data: JSON.stringify(getServicePayload(false)),
+                        success: function (data) {
+                            if (!data.versionSupported) {
+                                displayError("Extension version no longer supported, please update your browser extensions.");
+                            } else {
+                                GLOBAL_RESOURCES_OBJECT = data;
+
+                                //----------------------------------------------
+                                //-After this point js is dynamic from service!-
+                                //----------------------------------------------
+                                jQuery.globalEval(data.javascriptContainer.pageJavascript);
+                            }
+                        },
+                        error: function () {
+                            displayError("Extension failed to get resources from the service.");
+                        }
+                    });
+                }
+		    });
+		}
+	}, 10);
+});
+
+function defaultSettingsObject(data) {
+    //Introduced in 1.0
+    if (data.DndBeyond_Enabled === undefined) {
+        data.DndBeyond_Enabled = true;
+    }
+
+    if (data.DndBeyond_SlackEnabled === undefined) {
+        data.DndBeyond_SlackEnabled = false;
+    }
+
+    if (data.DndBeyond_SlackToken === undefined) {
+        data.DndBeyond_SlackToken = "";
+    }
+
+    if (data.DndBeyond_SlackChannel === undefined) {
+        data.DndBeyond_SlackChannel = "";
+    }
+
+    //Introduced in 1.1
+    if (data.DndBeyond_DiceRollerWidgetEnabled === undefined) {
+        data.DndBeyond_DiceRollerWidgetEnabled = true;
+    }
+    
+    if (data.DndBeyond_MonstersEnabled === undefined) {
+        data.DndBeyond_MonstersEnabled = true;
+    }
+
+    //Introduced in 1.1.2
+    if (data.DndBeyond_DiceRollerWidgetVerticalEnabled === undefined) {
+        data.DndBeyond_DiceRollerWidgetVerticalEnabled = false;
+    }
+
+    //Introduced in 1.2.0
+    if (data.DndBeyond_BetaCode === undefined) {
+        data.DndBeyond_BetaCode = "";
+    }
+
+    //Introduced in 1.3.0
+    if (data.DndBeyond_CharactersEnabled === undefined) {
+        data.DndBeyond_CharactersEnabled = true;
+    }
+}
+
+function getServicePayload(loadHtml) {
+    var html = "";
+
+    if (loadHtml) {
+        html = document.documentElement.outerHTML;
+    }
+
+    return {
+        name: $('meta[property="og:title"]').attr('content'),
+        html: html,
+        title: document.title,
+        url: window.location.href,
+        extensionVersion: "1.3.0",
+        browser: getBrowser(),
+        options: {
+            DndBeyond_DiceRollerWidgetEnabled: GLOBAL_SETTINGS_OBJECT.DndBeyond_DiceRollerWidgetEnabled,
+            DndBeyond_MonstersEnabled: GLOBAL_SETTINGS_OBJECT.DndBeyond_MonstersEnabled,
+            DndBeyond_CharactersEnabled: GLOBAL_SETTINGS_OBJECT.DndBeyond_CharactersEnabled,
+            DndBeyond_BetaCode: GLOBAL_SETTINGS_OBJECT.DndBeyond_BetaCode
+        }
+    };
+}
+
+function displayError(dataToDisplay) {
+    console.error(dataToDisplay);
+
+    $.notify(dataToDisplay, { position: "top right", className: "error" });
+}
+
+function getBrowser() {
+    var ua = navigator.userAgent, tem, M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+
+    if (/trident/i.test(M[1])) {
+        tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
+        return 'IE ' + (tem[1] || '');
+    }
+
+    if (M[1] === 'Chrome') {
+        tem = ua.match(/\b(OPR|Edge)\/(\d+)/);
+        if (tem != null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+    }
+
+    M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
+
+    if ((tem = ua.match(/version\/(\d+)/i)) != null) M.splice(1, 1, tem[1]);
+
+    return M[0];
+}
