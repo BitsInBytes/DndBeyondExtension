b4a122495dddf0be7c66bf2c79f1dc614bf4cffc
BitsInBytes <landinmartens@gmail.com>
2018-10-14 20:09:57 -0500

    Improvements to the dice roller HTML

--- Extensions/Chrome/JS/Extension/DiceApi/DiceRoller.js
+++ Extensions/Chrome/JS/Extension/DiceApi/DiceRoller.js
@@ -33,16 +33,6 @@
 
     PublishExecutedAttackRoll(attackRoll)
     {
-        if(this.Settings.DndBeyond_SlackEnabled)
-        {
-            //Nothing for now
-        }
-
-        this.DisplayExecutedAttackRoll(attackRoll);
-    }
-    
-    DisplayExecutedAttackRoll(attackRoll)
-    {
         var html = `<strong><i>${attackRoll.Title.toTitleCase()}</i></strong>`;
 
         html = `${html}<br /><br />${this.GetRollResultHtml(attackRoll.HitRoll.Result)}`;
@@ -81,7 +71,7 @@
             })
         }
 
-        this.PopUp(html);
+        this.DisplayHtmlBlocks([html]);
     }
 
     GetRollResultHtml(result)
@@ -125,132 +115,98 @@
         return `<span style="background-color:Honeydew; color:deeppink; font-size: 80%;">${object.ToString()}</span>`;
     }
 
-    PopUp(html)
+    DisplayHtmlBlocks(htmlBlocks)
     {
-        if (this.Settings.DndBeyond_SlackEnabled)
+        //TODO: Invoke slack integration here
+
+        var dialogOptions =
         {
-            var slackMarkUp = html.replaceAll("<strong>", "*");
-            slackMarkUp = slackMarkUp.replaceAll("</strong>", "*");
-            slackMarkUp = slackMarkUp.replaceAll("</i>", "_");
-            slackMarkUp = slackMarkUp.replaceAll("<i>", "_");
-            slackMarkUp = slackMarkUp.replaceAll("<br />", "\n");
-            slackMarkUp = slackMarkUp.replaceAll("<span style=\"background-color:Aqua; padding-right:3px; margin-right:10px;\" />", ">");
-            slackMarkUp = slackMarkUp.replaceAll("<span style=\"background-color:#01FF70; padding-right:3px; margin-right:10px;\" />", ">");
-            slackMarkUp = slackMarkUp.replaceAll("<span style=\"background-color:Honeydew; color:deeppink; font-size: 80%;\">", "`");
-            slackMarkUp = slackMarkUp.replaceAll("</span>", "`");
-    
-            var name = "N/A";
-            var icon = "";
-    
-            if (GLOBAL_CHARACTER_OBJECT.Exists)
+            "modal": true,
+            "resizable": false,
+            "draggable": true,
+            width: "auto",
+            hide:
             {
-                name = GLOBAL_CHARACTER_OBJECT.Name;
-                icon = GLOBAL_CHARACTER_OBJECT.Icon;
-            }
-            else if (GLOBAL_MONSTER_OBJECT.Exists)
+                effect: 'show',
+                duration: 250
+            },
+            hide:
             {
-                name = GLOBAL_MONSTER_OBJECT.Name;
-                icon = GLOBAL_MONSTER_OBJECT.Icon;
-            }
-            else if (GLOBAL_WIDGET_OBJECT.Exists)
+                effect: 'fade',
+                duration: 250
+            },
+            open: function ()
             {
-                name = GLOBAL_WIDGET_OBJECT.Name;
-                icon = GLOBAL_WIDGET_OBJECT.Icon;
-            }
-    
-            var fd = new FormData();
-            fd.append("token", this.Settings.DndBeyond_SlackToken);
-            fd.append("channel", this.Settings.DndBeyond_SlackChannel);
-            fd.append("text", slackMarkUp);
-            fd.append("as_user", "false");
-            fd.append("username", name);
-            fd.append("icon_url", icon);
-    
-            $.ajax(
-            {
-                url: 'https://slack.com/api/chat.postMessage',
-                data: fd,
-                processData: false,
-                contentType: false,
-                type: 'POST',
-                success: function (data)
+                var popup = $(this);
+
+                $('.ui-widget-overlay').bind('click', function ()
                 {
-                    if (data.ok === true)
-                    {
-                        displaySuccess("Slack message sent successfully!");
-                    }
-                    else
-                    {
-                        displayError("Slack message failed with response!");
-                        console.log(data);
-                    }
-                },
-                error: function ()
+                    popup.dialog('close');
+                });
+
+                $('.DiceRollResultsRollAgainButton').bind('click', function ()
                 {
-                    displayError("Slack message failed!");
-                }
-            });
-        }
-        else
+                    popup.dialog('close');
+                    //this.ExecuteActions(actions);
+                });
+            },
+        };
+
+        var generatedHtml = "";
+        htmlBlocks.forEach(html =>
         {
-            var dialogOptions =
+            if(generatedHtml === "")
             {
-                "modal": true,
-                "resizable": false,
-                "draggable": true,
-                width: "auto",
-                hide:
-                {
-                    effect: 'show',
-                    duration: 250
-                },
-                hide:
-                {
-                    effect: 'fade',
-                    duration: 250
-                },
-                open: function ()
-                {
-                    var popup = $(this);
-    
-                    $('.ui-widget-overlay').bind('click', function ()
-                    {
-                        popup.dialog('close');
-                    });
-    
-                    $('.DiceRollResultsRollAgainButton').bind('click', function ()
-                    {
-                        popup.dialog('close');
-                        //this.ExecuteActions(actions);
-                    });
-                },
-            };
-    
-            //Create dialog
-            var dialog = $('<div></div>').html(html + '<br /><center><button class="character-button-block DiceRollResultsRollAgainButton" style="width:120px;">Roll Again</button></center>').dialog(dialogOptions);
-            var dialogContainer = dialog.parent();
-    
-            //Bring to z-top most
-            dialogContainer.style('z-index', '999999999999', 'important');
-    
-            //Set header bar size
-            var titleBar = dialogContainer.find(".ui-dialog-titlebar");
-            titleBar.css('height', '2px');
-            titleBar.css('padding', '0px');
-    
-            //Set header text
-            titleBar.html('<span id="ui-id-2" class="ui-dialog-title" style="float: left; margin-top:2px;"><img src="' + chrome.extension.getURL("images/icon48.png") + '" width="20" height="20" style="margin-bottom:5px; margin-left: 5px;" alt="D&amp;D Beyond Interaction" title="D&amp;D Beyond Interaction"/>  Dice Roll Results</span>');
-    
-            //Header images
-            var closeImage = '<img src="' + chrome.extension.getURL("images/closeicon.png") + '" width="15" height="15" alt="X" title="Close"/>';
-    
-            //Create header buttons
-            dialogContainer.find(".ui-dialog-titlebar").append('<button type="button" class="widget-close-button ui-button ui-corner-all ui-widget ui-button-icon-only" title="Close" style="background-color: #bf3333; padding:4px; float: right; margin-top:4px; margin-right: 5px;">' + closeImage + '</button>');
-    
-            dialogContainer.find('.widget-close-button').click(function ()
+                generatedHtml = `<td>${html}</td>`;
+            }
+            else
             {
-                dialog.dialog('close');
-            });
-        }
+                generatedHtml = `${generatedHtml}<td style="padding-left:50px;">${html}</td>`;
+            }
+        });
+
+        //Dialog inner html
+        var dialogInnerHtml =
+        `
+        <table style="width: 100%;">
+            <tr>
+                ${generatedHtml}
+            </tr>
+            <tr>
+                <td colspan="${htmlBlocks.length}">
+                    <br />
+                    <center>
+                        <button class="character-button-block DiceRollResultsRollAgainButton" style="width:120px;">Roll Again</button>
+                    </center>
+                </td>
+            </tr>
+        </table>
+        `;
+
+        //Create dialog
+        var dialog = $('<div></div>').html(dialogInnerHtml).dialog(dialogOptions);
+        var dialogContainer = dialog.parent();
+
+        //Bring to z-top most
+        dialogContainer.style('z-index', '999999999999', 'important');
+
+        //Set header bar size
+        var titleBar = dialogContainer.find(".ui-dialog-titlebar");
+        titleBar.css('height', '2px');
+        titleBar.css('padding', '0px');
+
+        //Set header text
+        titleBar.html('<span id="ui-id-2" class="ui-dialog-title" style="float: left; margin-top:2px;"><img src="' + chrome.extension.getURL("images/icon48.png") + '" width="20" height="20" style="margin-bottom:5px; margin-left: 5px;" alt="D&amp;D Beyond Interaction" title="D&amp;D Beyond Interaction"/>  Dice Roll Results</span>');
+
+        //Header images
+        var closeImage = '<img src="' + chrome.extension.getURL("images/closeicon.png") + '" width="15" height="15" alt="X" title="Close"/>';
+
+        //Create header buttons
+        dialogContainer.find(".ui-dialog-titlebar").append('<button type="button" class="widget-close-button ui-button ui-corner-all ui-widget ui-button-icon-only" title="Close" style="background-color: #bf3333; padding:4px; float: right; margin-top:4px; margin-right: 5px;">' + closeImage + '</button>');
+
+        dialogContainer.find('.widget-close-button').click(function ()
+        {
+            dialog.dialog('close');
+        });
     }
 }
--- Extensions/Chrome/js/extension/Initialize.js
+++ Extensions/Chrome/js/extension/Initialize.js
@@ -33,7 +33,8 @@ var attackRoll =       new AttackContainer(
             new DamageRoll(1,4,1,"second",new SavingThrow(20, "wis"))]
         ),
     ],
-    [new Roll(1,4,0,"days"),new Roll(1,12,0,"hours")]
+    [new Roll(1,4,0,"days"),new Roll(1,12,0,"hours")],
+    `Frightful Presence. Each creature of the dragon's choice that is within 120 feet of the dragon and aware of it must succeed on a DC 19 Wisdom saving throw or become frightened for 1 minute. A creature can repeat the saving throw at the end of each of its turns, ending the effect on itself on a success. If a creature's saving throw is successful or the effect ends for it, the creature is immune to the dragon's Frightful Presence for the next 24 hours.`
     );
 
                 var executedRoll = dr.ExecuteAttackRoll(attackRoll);
